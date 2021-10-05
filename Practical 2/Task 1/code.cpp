2e0332e6a5630fdcf8bd91983ff6e49487456d75
Abhishek Bhatt <86089661+AbhishekBhatt072003@users.noreply.github.com>
2021-10-05 18:34:57 +0530

    Delete Practical 2 directory

--- Practical 2/Task 1/code.cpp
+++
@@ -1,7 +0,0 @@
-using namespace std;
-
-int main()
-{
-    cout << "This is the task 1 of practical 2";
-    return 0;
-}
--- Practical 2/Task 2/code.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream.h>
-using namespace std;
-
-int main()
-{
-    cout <<"This is the task 2 of practical 2";
-    return 0;
-}
